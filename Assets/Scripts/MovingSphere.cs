271b39eadab86c9f00e5835bcd9f369f623ca507
asseternity <nakupovasset@gmail.com>
2024-03-30 16:27:20 +0500

    Player movement part 2

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -2,5 +2,20 @@ using UnityEngine;
 
 public class MovingSphere : MonoBehaviour
 {
-    
+    void Update()
+    {
+        Vector2 playerInput;
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+
+        [SerializeField, Range(0f, 100f)]
+        float maxSpeed = 10f;
+        Vector3 velocity;
+        Vector3 acceleration = 
+            new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        velocity += acceleration * Time.deltaTime;
+        Vector3 displacement = velocity * Time.deltaTime;
+        transform.localPosition += displacement;
+    }    
 }
